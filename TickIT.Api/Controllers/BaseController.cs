3af9a198623b8c2370414011929ecdd571ba7070
Nagababu Thota <nagababuthota593@gmail.com>
2023-06-24 13:03:11 +0530

    added api project

---
+++ TickIT.Api/Controllers/BaseController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TickIT.Core;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TickIT.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+        public readonly HostService _hostService;
+        public BaseController()
+        {
+        }
+    }
+}
---
+++ TickIT.Api/Controllers/TicketController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TickIT.Core;
+
+namespace TickIT.Api.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    [Authorize]
+    public class TicketController : BaseController
+    {
+        [HttpGet("tickets")]
+        public IActionResult GetAll()
+        {
+            HostService hostService = HttpContext.RequestServices.GetService<HostService>();
+
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
